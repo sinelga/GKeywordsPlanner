cc52e65496b125a35b46ea0812a6e725355de675
Sinelga <support@sinelga.com>
2014-04-24 13:26:50 +0300

    prod

--- GKeywordsPlanner/usefull.bash
+++ GKeywordsPlanner/usefull.bash
@@ -3,6 +3,8 @@
 
 bin/keywordsplannerApp -locale=it_IT -themes=finance
 bin/setphrasesforprod -locale=it_IT -themes=finance
+#rm csvfiles/it_IT_finance/*.csv
+
 
 bin/keywordsplannerApp -locale=fi_FI -themes=finance
 bin/setphrasesforprod -locale=fi_FI -themes=finance
